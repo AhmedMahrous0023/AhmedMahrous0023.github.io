4eb69e1874082e640057d263d740afba45d765cc
Ahmed Mahrous <147205485+AhmedMahrous0023@users.noreply.github.com>
2026-03-21 05:59:12 +0200

    Create main.js

---
+++ javascript/main.js
@@ -0,0 +1,26 @@
+
+/* ================= JS FILE (js/main.js) ================= */
+
+AOS.init();
+
+let isArabic = false;
+
+const toggle = document.getElementById('langToggle');
+
+toggle.onclick = () => {
+  isArabic = !isArabic;
+
+  if(isArabic){
+    document.getElementById('title').innerText = 'مرحبا، أنا أحمد محروس';
+    document.getElementById('subtitle').innerText = 'مطور تطبيقات Flutter';
+    document.getElementById('cta').innerText = 'شاهد أعمالي';
+    toggle.innerText = 'EN';
+    document.body.dir = 'rtl';
+  } else {
+    document.getElementById('title').innerText = "Hi, I'm Ahmed Mahrous";
+    document.getElementById('subtitle').innerText = 'Flutter Developer building modern mobile apps';
+    document.getElementById('cta').innerText = 'View My Work';
+    toggle.innerText = 'AR';
+    document.body.dir = 'ltr';
+  }
+};
